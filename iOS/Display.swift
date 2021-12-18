e0e15df9bf21c7f4c0b101c46562437cfe9d705a
vaux <zero.griffin@gmail.com>
2021-12-18 14:25:59 +0100

    Add altitude

--- iOS/Display.swift
+++ iOS/Display.swift
@@ -7,15 +7,31 @@ struct Display: View {
     @State private var moon: Moon?
     @State private var slider = Float(0)
     @State private var test = Date.now
+    @State private var segmented = 0
     private let location = Coords(coordinate: .init(latitude: 52.498252, longitude: 13.423622))
     private let date = Date.now
     private let observatory = Observatory()
 
     var body: some View {
         VStack {
-            if let moon = moon {
-                Azimuth(moon: moon)
+            Picker("Display", selection: $segmented) {
+                Text("Azimuth")
+                    .tag(0)
+                Text("Altitude")
+                    .tag(1)
             }
+            .pickerStyle(.segmented)
+            .padding(.horizontal)
+            
+            if let moon = moon {
+                switch segmented {
+                case 0:
+                    Azimuth(moon: moon)
+                default:
+                    Altitude(moon: moon)
+                }
+            }
+            
             Text(test, format: .dateTime)
             Slider(value: $slider, in: -48 ... 48)
                 .padding(.horizontal)
@@ -32,6 +48,8 @@ struct Display: View {
     private func update() {
         Task {
             moon = await observatory.moon(input: .init(date: test, coords: location))
+            
+            print(moon?.azimuth)
         }
     }
 }
