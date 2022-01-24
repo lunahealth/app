a827d7d94691a9df7ad1157b2295e6defe44eec5
vaux <zero.griffin@gmail.com>
2022-01-24 18:48:19 +0100

    Use regular material

--- iOS/Track.swift
+++ iOS/Track.swift
@@ -57,7 +57,7 @@ struct Track: View {
             }
         }
         .frame(maxWidth: .greatestFiniteMagnitude)
-        .background(.thinMaterial)
+        .background(.regularMaterial)
         .sheet(isPresented: $status.preferences, content: Settings.Traits.init)
     }
 }
