3c6ab749a31ee96771323d4368d5dfed35f9867c
vaux <zero.griffin@gmail.com>
2022-01-27 18:46:20 +0100

    Update calendar

--- iOS/Cal.Ring.swift
+++ iOS/Cal.Ring.swift
@@ -5,7 +5,7 @@ import Selene
 extension Cal {
     struct Ring: View {
         weak var observatory: Observatory!
-        let calendar: [Days<Journal>.Item]
+        let month: [Days<Journal>.Item]
         private let moonImage = Image("MoonMini")
         private let shadowImage = Image("ShadowMini")
         
@@ -13,39 +13,47 @@ extension Cal {
             Canvas { context, size in
                 let radius = min(size.width, size.height) * 0.5
                 let center = CGPoint(x: size.width / 2, y: size.height / 2)
-                
-                context.stroke(.init {
-                    $0.addArc(center: center,
-                              radius: radius - 31,
-                              startAngle: .radians(0),
-                              endAngle: .radians(.pi2 + 0.1),
-                              clockwise: false)
-                }, with: .color(.accentColor.opacity(0.35)), style: .init(lineWidth: 56))
-
-                context.stroke(.init {
-                    $0.addArc(center: center,
-                              radius: radius - 45,
-                              startAngle: .radians(0),
-                              endAngle: .radians(.pi2 + 0.1),
-                              clockwise: false)
-                }, with: .color(.init("Path").opacity(0.3)), style: .init(lineWidth: 28))
-
-                let rad = Double.pi2 / .init(calendar.count)
+                let rad = Double.pi2 / .init(month.count)
                 let half = rad / 2
-                var rotation = -rad
+                let start = -(.pi_2 - 0.01)
+                let end = (start + rad) - 0.02
+                var rotation = -(rad + half)
                 
-                calendar
+                month
                     .forEach { day in
                         rotation += rad
-
+                        let date = day.content.date
+                        
                         context.translateBy(x: center.x, y: center.y)
                         context.rotate(by: .radians(rotation))
                         context.translateBy(x: -center.x, y: -center.y)
-
-                        context.stroke(.init {
-                            $0.move(to: .init(x: center.x, y: center.y - 95))
-                            $0.addLine(to: .init(x: center.x, y: center.y - radius))
-                        }, with: .color(.primary.opacity(0.4)), style: .init(lineWidth: 1, dash: [1, 3, 3, 5]))
+                        
+                        if date <= .now {
+                            context.stroke(.init {
+                                $0.addArc(center: center,
+                                          radius: radius - 31,
+                                          startAngle: .radians(start),
+                                          endAngle: .radians(end),
+                                          clockwise: false)
+                            }, with: .color(.accentColor.opacity(day.today ? 1 : 0.25)),
+                                           style: .init(lineWidth: 56, lineCap: .butt))
+                            
+                            context.stroke(.init {
+                                $0.addArc(center: center,
+                                          radius: radius - 45,
+                                          startAngle: .radians(start),
+                                          endAngle: .radians(end),
+                                          clockwise: false)
+                            }, with: .color(.init("Path").opacity(day.today ? 0.5 : 0.25)),
+                                           style: .init(lineWidth: 28, lineCap: .butt))
+                            
+                        } else if date >= Calendar.global.date(byAdding: .day, value: 1, to: .now)! {
+                            context.stroke(.init {
+                                $0.move(to: .init(x: center.x, y: center.y - 90))
+                                $0.addLine(to: .init(x: center.x, y: center.y - radius))
+                            }, with: .color(.primary.opacity(0.3)),
+                                           style: .init(lineWidth: 1, dash: [1, 3, 3, 5]))
+                        }
 
                         context.translateBy(x: center.x, y: center.y)
                         context.rotate(by: .radians(half))
@@ -57,8 +65,8 @@ extension Cal {
                             con.translateBy(x: center.x, y: center.y)
                             con.rotate(by: .radians(-rotation))
                             con.translateBy(x: -center.x, y: -center.y)
-                            
-                            con.draw(moon: observatory.moon(for: day.content.date),
+                            con.opacity = date <= .now ? 1 : 0.65
+                            con.draw(moon: observatory.moon(for: date),
                                          image: moonImage,
                                          shadow: shadowImage,
                                          radius: 7,
@@ -66,7 +74,9 @@ extension Cal {
                         }
 
                         context.draw(Text(day.value.formatted())
-                                        .font(.system(size: 11).monospacedDigit()), at: .init(x: center.x, y: center.y - radius + 46))
+                                        .font(.system(size: 11).monospacedDigit())
+                                        .foregroundColor(date <= .now ? .primary : .secondary),
+                                     at: .init(x: center.x, y: center.y - radius + 46))
 
                         context.translateBy(x: center.x, y: center.y)
                         context.rotate(by: .radians(-(rotation + half)))
--- iOS/Cal.swift
+++ iOS/Cal.swift
@@ -13,15 +13,13 @@ struct Cal: View {
                 .aspectRatio(contentMode: .fill)
                 .edgesIgnoringSafeArea(.all)
             
-            VStack {
-                HStack {
-                    Text("January 2022")
-                }
-                .padding(.top, 30)
-                Spacer()
+            if let month = calendar.last {
+                Text(Calendar.current.date(from: .init(year: month.year, month: month.month))!,
+                     format: .dateTime.year().month(.wide))
+                    .font(.footnote)
+                
+                Ring(observatory: observatory, month: month.items.flatMap { $0 })
             }
-            
-            Ring(observatory: observatory, calendar: calendar.flatMap { $0.items.flatMap { $0 } })
         }
         .task {
             calendar = await cloud.model.calendar
