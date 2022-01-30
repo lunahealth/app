2330c92b794cd30a1d60ebaf090c32e63c70ea3b
vaux <zero.griffin@gmail.com>
2022-01-30 16:13:56 +0100

    Update analysis

--- iOS/Analysis.Item.Display.swift
+++ iOS/Analysis.Item.Display.swift
@@ -13,13 +13,56 @@ extension Analysis.Item {
                 var x = width / 2
                 
                 Moon.Phase.allCases.forEach { phase in
-                    context.drawLayer { layer in
-                        layer.draw(phase: phase,
-                                   image: moonImage,
-                                   shadow: shadowImage,
-                                   radius: 7,
-                                   center: .init(x: x, y: 180))
-                    }
+                    let bottom = CGPoint(x: x, y: size.height - 20)
+                    let top = CGPoint(x: x, y: 50)
+                    
+                    context
+                        .drawLayer { layer in
+                            layer.addFilter(.blur(radius: 6))
+                            
+                            layer
+                                .stroke(.init {
+                                    $0.move(to: bottom)
+                                    $0.addLine(to: top)
+                                }, with: .linearGradient(.init(colors: [.accentColor, .clear]),
+                                                         startPoint: bottom,
+                                                         endPoint: top),
+                                        style: .init(lineWidth: 8, lineCap: .round))
+
+                            layer
+                                .fill(.init {
+                                    $0.addEllipse(in: .init(x: bottom.x - 30, y: bottom.y - 6, width: 60, height: 12))
+                                }, with: .radialGradient(.init(stops: [.init(color: .accentColor, location: 0),
+                                                                       .init(color: .clear, location: 1)]),
+                                                         center: bottom,
+                                                         startRadius: 0,
+                                                         endRadius: 30,
+                                                         options: .linearColor))
+                            
+                            layer
+                                .fill(.init {
+                                    $0.addArc(center: bottom,
+                                              radius: 10,
+                                              startAngle: .radians(0),
+                                              endAngle: .radians(.pi2),
+                                              clockwise: false)
+                                }, with: .radialGradient(.init(stops: [.init(color: .accentColor, location: 0),
+                                                                       .init(color: .clear, location: 1)]),
+                                                         center: bottom,
+                                                         startRadius: 0,
+                                                         endRadius: 10,
+                                                         options: .linearColor))
+                        }
+                    
+                    context
+                        .drawLayer { layer in
+                            layer.draw(phase: phase,
+                                       image: moonImage,
+                                       shadow: shadowImage,
+                                       radius: 7,
+                                       center: bottom)
+                        }
+                    
                     x += width
                 }
             }
