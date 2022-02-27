3f669c6bf0880120a3f7aaeb27e2cae8d481e887
vaux <zero.griffin@gmail.com>
2022-02-27 13:06:27 +0100

    Remove dead variable

--- iOS/App.swift
+++ iOS/App.swift
@@ -3,7 +3,6 @@ import SwiftUI
 @main struct App: SwiftUI.App {
     @Environment(\.scenePhase) private var phase
     @UIApplicationDelegateAdaptor(Delegate.self) private var delegate
-    @State private var a = ""
     
     var body: some Scene {
         WindowGroup {
