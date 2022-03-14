e333ea4d38c4976553729f30f1aef696de5ee384
vaux <zero.griffin@gmail.com>
2022-03-14 15:16:18 +0100

    Update stats

--- iOS/Analysis.swift
+++ iOS/Analysis.swift
@@ -26,7 +26,14 @@ struct Analysis: View, Equatable {
                             .padding(.top, 30)
                     } else {
                         if let trait = trait, let analysis = analysis[trait] {
-                            Chart(trait: trait, value: analysis)
+                            Chart(trait: trait, value: [.new : .bottom,
+                                                        .waxingCrescent : .low,
+                                                        .firstQuarter : .top,
+                                                        .waxingGibbous : .high,
+                                                        .full : .top,
+                                                        .waningGibbous : .medium,
+                                                        .lastQuarter : .high,
+                                                        .waningCrescent : .high])
                                 .equatable()
                                 .id(analysis)
                         }
@@ -71,7 +78,11 @@ struct Analysis: View, Equatable {
                         .padding(.top)
                     } else {
                         if let trait = trait {
-                            Info(trait: trait, stats: stats)
+                            Info(trait: trait, stats: [.init(level: .high, percent: 0.25),
+                                                       .init(level: .top, percent: 0.16),
+                                                       .init(level: .medium, percent: 0.14),
+                                                       .init(level: .low, percent: 0.1),
+                                                       .init(level: .bottom, percent: 0.05)])
                                 .animation(.easeInOut(duration: 0.3), value: stats)
                         }
                     }
