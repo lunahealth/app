bfbed3addfe177a2ad5fe6bc5041a42ae172432a
vaux <zero.griffin@gmail.com>
2022-03-15 09:57:22 +0100

    Remove shadow small

--- Shared/Render.swift
+++ Shared/Render.swift
@@ -7,6 +7,5 @@ struct Render {
     let blur: Bool
     
     static let regular = Self(image: .init("Moon"), shadow: .init("Shadow"), radius: 34, blur: true)
-    static let small = Self(image: .init("MoonSmall"), shadow: .init("ShadowSmall"), radius: 18, blur: false)
     static let mini = Self(image: .init("MoonMini"), shadow: .init("ShadowMini"), radius: 8, blur: false)
 }
