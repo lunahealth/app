06edc1053a14ee5929e8de84c52b4cbd3619b9be
vaux <zero.griffin@gmail.com>
2022-03-16 17:55:36 +0100

    Update watch

--- watchOS Extension/Track.swift
+++ watchOS Extension/Track.swift
@@ -1,11 +1,38 @@
 import SwiftUI
+import Selene
 
 struct Track: View {
+    @State private var traits = [Trait]()
+    @State private var journal: Journal?
+    
     var body: some View {
         NavigationView {
-            List {
-                
+            if traits.isEmpty {
+                Text("Set your preferences\nto start tracking")
+                    .font(.footnote)
+                    .multilineTextAlignment(.center)
+            } else {
+                List(traits, id: \.self) { trait in
+                    
+                }
             }
+        }
+        .onReceive(cloud) { model in
+            traits = Trait
+                .allCases
+                .filter {
+                    model.settings.traits.contains($0)
+                }
+                .sorted()
+            
+            refresh()
+        }
+        .onAppear(perform: refresh)
+    }
+    
+    private func refresh() {
+        Task {
+            journal = await cloud.model[.now]
         }
     }
 }
